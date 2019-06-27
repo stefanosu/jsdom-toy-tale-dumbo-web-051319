e57ee20a4dbcbfbdc25efa4c5e31ad4512d10b63
Stefanos <stefanosu422@gmail.com>
2019-06-27 18:55:29 -0400

    almost there

--- src/index.js
+++ src/index.js
@@ -15,5 +15,87 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+function addToyCard(toy, divCollection) {
+  const div = document.createElement('div') 
+  div.className = "card"
+  div.innerHTML = ` 
+  <h2> ${toy.name} </h2>
+  <img src= ${toy.image} class = 'toy-avatar' />
+  <p> ${toy.likes} Likes </p>
+  <button data-id = ${toy.id} class= "like-btn">Like<3</button>
+  `  
 
-// OR HERE!
+  const likeBtn = div.querySelector(".like-btn") 
+  likeBtn.addEventListener("click", function(event){
+    toy.likes++;
+
+  })
+  divCollection.append(div)
+}
+
+
+
+
+ function makeToys(toys) {
+  const divCollection = document.querySelector('#toy-collection')
+  toys.forEach(function(toy){addToyCard(toy, divCollection)
+  })  
+ } 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('http://localhost:3000/toys') 
+    .then(response => response.json()) 
+    .then(makeToys)
+})      
+
+
+const form = document.querySelector('.add-toy-form')
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+    fetch('http://localhost:3000/toys',{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify( 
+      {
+        "name": form.name.value,
+        "image": form.image.value,
+        "likes": 0
+      })
+    })
+    .then(response => response.json())
+    .then(addToyCard)
+  })   
+
+
+  likeBtn.addEventListener("click", function(event){
+      const id = event.target.dataset.id 
+      fetch('http://localhost:3000/toys/id', {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json", 
+          Accept: "application/json"
+        }, 
+        body: JSON.stringify( 
+          "likes"
+
+        )
+      }) 
+    
+  
+function updateLike(event) {
+  const id = event.target.dataset.id
+  fetch('http://localhost:3000/toys/${id}', {
+    method: "PATCH", 
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+
+    })
+  })
+}
